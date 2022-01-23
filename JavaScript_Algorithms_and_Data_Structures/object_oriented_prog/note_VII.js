e41e9678a967727ab7d751cf4b699c105aa33804
ShubhrankR <shubhrankrastogi@gmail.com>
2022-01-24 01:22:50 +0530

    Completed OOP

---
+++ JavaScript_Algorithms_and_Data_Structures/object_oriented_prog/note_VII.js
@@ -0,0 +1,19 @@
+/**
+ * Use an IIFE to Create a Module
+ * ===============================
+ */
+
+let funModule = (function () {
+    return {
+        isCuteMixin: function (obj) {
+            obj.isCute = function () {
+                return true;
+            };
+        },
+        singMixin: function (obj) {
+            obj.sing = function () {
+                console.log("Singing to an awesome tune");
+            };
+        },
+    };
+})();
