de1e0654e048cda790ddeb3f47d76a92e6c2d7df
ShubhrankR <shubhrankrastogi@gmail.com>
2022-03-08 09:55:32 +0530

    Completed

---
+++ JavaScript_Algorithms_and_Data_Structures/JavaScript_Algorithms_and_Data_Structures_Projects/question_V.js
@@ -0,0 +1,26 @@
+/**
+ * Cash Register
+ * ================
+ *
+ * Hints - 
+ * ---------
+ * - https://github.com/yazeedb/FCC-Algos-and-DS-Solutions/blob/master/cashRegister.js
+ * - https://medium.com/@chyanpin/solving-freecodecamp-orgs-final-project-challenge-cash-register-d6ecb941d966
+ * - https://www.youtube.com/watch?v=7uyZ8pBXmqs
+ */
+function checkCashRegister(price, cash, cid) {
+    let change;
+    return change;
+}
+
+checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+]);
