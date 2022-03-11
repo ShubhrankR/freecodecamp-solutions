8af8926cf04f61905c8894abc2d0ba4ada7904fc
apoorvachikara <chikara02apoorva@gmail.com>
2022-03-11 11:06:21 +0530

    Update question_VII.js
    - then and catch are chainable operators
    - catch is added on the same promise to catch the error if any.

--- JavaScript_Algorithms_and_Data_Structures/es6/question_VII.js
+++ JavaScript_Algorithms_and_Data_Structures/es6/question_VII.js
@@ -26,9 +26,15 @@ const makeServerRequest = new Promise((resolve, reject) => {
     }
 });
 
+// makeServerRequest.then((result) => {
+//     console.log(result);
+// });
+// makeServerRequest.catch((error) => {
+//     console.log(error);
+// });
+
 makeServerRequest.then((result) => {
     console.log(result);
-});
-makeServerRequest.catch((error) => {
-    console.log(error);
-});
+}).catch((e) => {
+    console.error(e);
+})
